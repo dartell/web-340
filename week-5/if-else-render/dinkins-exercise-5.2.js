8034dc34cce88862508330ccf3f3b4d2c5b0b390
Darius Dinkins <dtdinkins@my365.bellevue.edu>
2020-05-21 19:21:58 -0400

    Added exercise 5.2

---
+++ week-5/if-else-render/dinkins-exercise-5.2.js
@@ -0,0 +1,33 @@
+// requires
+var express = require("express");
+var http = require("http");
+var path = require ("path");
+
+// app functions
+var app = express();
+
+//remember, instructs express to look in views folder
+app.set("views", path.resolve(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// data array
+var f = [
+    "Bob",
+    "Zusan",
+    "Ralph",
+    "Melca"
+
+];
+
+//(routes) when the app is called render the array "f" under the names variable on the index page
+app.get("/", function(request, response) {
+    response.render("index", {
+        names: f
+
+    });
+});
+
+//create server with console.log message
+http.createServer(app).listen(8080, function(){
+    console.log("Application started on port 8080!");
+});
