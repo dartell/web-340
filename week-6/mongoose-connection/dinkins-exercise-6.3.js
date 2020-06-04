0b5b6648fddba10ab9e2bbec982229c371675050
Darius Dinkins <dtdinkins@my365.bellevue.edu>
2020-06-04 11:58:03 -0400

    Added exercise 6.3

---
+++ week-6/mongoose-connection/dinkins-exercise-6.3.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var http = require('http');
+var logger = require('morgan');
+var mongoose = require("mongoose");
+
+
+var mongoDB = "mongodb+srv://dartell:j3X4EnC7cZqNqlJ4@buwebdev-cluster-1-dy3io.mongodb.net/test";;
+
+mongoose.connect(mongoDB, {
+    useMongoClient: true
+
+});
+
+mongoose.Promise = global.Promise;
+
+var db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "MongoDB connected error:"));
+
+db.once("open", function() {
+    console.log("Application connected to mLab MongoDB instance");
+
+});
+
+var app = express();
+app.use(logger('dev'));
+
+http.createServer(app).listen(5000, function () {
+
+    console.log('Application started and listening on port 5000')
+})
