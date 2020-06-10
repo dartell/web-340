a3cd7574bbf5fc7c282beab84e9870e51b456629
Darius Dinkins <dtdinkins@my365.bellevue.edu>
2020-06-10 08:57:59 -0400

    Added Week 7 exercises and EMS

---
+++ week-7/tdd-example/test/dinkins-exercise-7.2.js
@@ -0,0 +1,15 @@
+var assert = require("assert");
+
+describe("String#split", function() {
+    
+    it("should return an array of fruits", function() {
+
+        assert(Array.isArray("Apple,Orange,Mango".split(',')));
+    });
+});
+
+function getFruits(str) {
+
+}
+
+module.exports = getFruits;
